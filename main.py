a082922b9ef300fc4afef28da2df431f43ced03f
librariandad <kengwall@gmail.com>
2024-10-30 16:02:54 -0500

    added count_chars function

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-    print(count_words(file_contents))
+    print(count_chars(file_contents))
 
-def count_words(chunk_o_text):
-    words = chunk_o_text.split()
+def count_words(text):
+    words = text.split()
     return len(words)
+
+def count_chars(text):
+    char_dict = {}
+    lowered_text = text.lower()
+    for char in lowered_text:
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+    return char_dict
+
 
 main()
